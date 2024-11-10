dd309fde329a1ff524a79da20b0511a140ee6713
Tanmay Joshi <tanmayjoshi072@gmail.com>
2024-11-10 19:44:09 +0530

    Some Small Bugs Has Been Small

--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,8 @@
 const express=require('express')
 const {Show}=require('./../controller/main')
 const router=express.Router()
+router.route('/').get((req,res)=>{
+    res.redirect('/Home')
+})
 router.route('/Home').get(Show)
 module.exports=router
