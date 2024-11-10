62f6d59c06d31ba43809e898b401bdeaa244809d
Tanmay Joshi <tanmayjoshi072@gmail.com>
2024-11-10 19:38:24 +0530

    40% done

--- app.js
+++ app.js
@@ -6,6 +6,6 @@ app.use('/JS', express.static(path.join(__dirname, 'public/JS')))
 app.use('/Images', express.static(path.join(__dirname, 'public/Images')))
 app.use('/CSS', express.static(path.join(__dirname, 'public/CSS')))
 app.use('/',home_routes)
-app.listen(8000,'127.0.0.1',()=>{
+app.listen(8000,'0.0.0.0',()=>{
     console.log('hey i am listening to this server bro')
 })
