03d78fbeabf65021dc8377d61673803d23f0652c
Tanmay Joshi <tanmayjoshi072@gmail.com>
2024-11-16 13:24:02 +0530

    55% done

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path=require('path')
 app.use('/JS', express.static(path.join(__dirname, 'public/JS')))
 app.use('/Images', express.static(path.join(__dirname, 'public/Images')))
 app.use('/CSS', express.static(path.join(__dirname, 'public/CSS')))
+app.use('/Videos', express.static(path.join(__dirname, 'public/Videos')))
 app.use('/',home_routes)
 app.listen(8000,'0.0.0.0',()=>{
     console.log('hey i am listening to this server bro')
